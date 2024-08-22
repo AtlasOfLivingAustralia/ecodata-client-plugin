1ae9fb8476705bc8e5c3cc03a21b8770c0f3efd7
chrisala <chris.godwin.ala@gmail.com>
2024-08-23 08:57:50 +1000

    Updated test #258

--- src/integration-test/groovy/pages/MultiInput.groovy
+++ src/integration-test/groovy/pages/MultiInput.groovy
@@ -6,6 +6,6 @@ class MultiInput extends Module {
     static content = {
         addItem(required: true) { $("multi-input i.fa-plus.fa") }
         inputItem(required: false) { $("multi-input input") }
-        removeItem(required: false) { $("multi-input span.add-on") }
+        removeItem(required: false) { $("multi-input span.input-group-text") }
     }
 }
