0ffc9cc1f1d1cfd567d32d56ed0b35dfeacbac87
chrisala <chris.godwin.ala@gmail.com>
2025-12-19 11:59:39 +1100

    Fixed default value for new editable param #326

--- grails-app/assets/components/javascript/multi-input.js
+++ grails-app/assets/components/javascript/multi-input.js
@@ -2,7 +2,7 @@ ko.components.register('multi-input', {
     viewModel: function(params) {
         var self = this;
 
-        self.editable = params.editable;
+        self.editable = _.isUndefined(params.editable) ? true : params.editable;
         self.observableValues = ko.observableArray();
         self.addValueText = params.addValueText;
 
